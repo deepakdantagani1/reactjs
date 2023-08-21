31b90c00c0e2543813bd465c5be29622aad69f48
deepakdantagani1 <132472072+deepakdantagani1@users.noreply.github.com>
2023-08-21 00:20:28 -0400

    controlled components done

---
+++ ReactNanodegree/2-StateManagement/4-ControlledComponents.js
@@ -0,0 +1,8 @@
+/**
+ * Controlled Components refer to components that render a form, but the "source of truth" for that form state 
+ * lives inside of the component state rather than inside of the DOM. The benefits of Controlled Components are:
+ * 
+ * Instant input validation
+ * Conditionally enable or disable buttons
+ * Enforce input formats
+ */
